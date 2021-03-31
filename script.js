085d6e313eb191050902cd1fc20a92b7c66a5c5a
mirabiliss <yanahill29@gmail.com>
2021-03-31 12:57:22 +0300

    created a pie chart

--- script.js
+++ script.js
@@ -42,3 +42,67 @@ function startTimer(duration, display) {
       }
   }, 1000);
 }
+
+function drawChart() {
+  let dataCounts = []
+  let dataHistory = []
+  dataHistory.push('wr');
+  dataHistory.push('wb');
+  dataHistory.push('wp');
+  dataHistory.push('wk');
+  dataHistory.push('wn');
+  dataHistory.push('wq');
+  dataHistory.push('bb');
+  dataHistory.push('bn');
+  dataHistory.push('bp');
+  dataHistory.push('br');
+  dataHistory.push('bq');
+  dataHistory.push('bk');
+
+  for (let i = 0; i < dataHistory.length; i++) {
+    dataCounts.push(document.getElementsByClassName(`${dataHistory[i]}`).length);
+  }
+
+  var ctx = document.getElementById('chart');
+  
+  new Chart(ctx, {
+    type: 'pie',
+    data: {
+      labels: [...dataHistory],
+      datasets: [{
+        borderColor: [
+          'rgb(244, 10, 10)',
+          'rgb(248, 129, 32)',
+          'rgb(247, 216, 43)', 
+          'rgb(248, 243, 124)',
+          'rgb(128, 248, 124)',
+          'rgb(13, 160, 44)',
+          'rgb(157, 250, 233)',
+          'rgb(51, 199, 244)',
+          'rgb(9, 52, 239)',
+          'rgb(141, 27, 247)',
+          'rgb(200, 27, 247)',
+          'rgb(249, 51, 206)',
+          'rgb(244, 10, 10)'
+          ],
+        backgroundColor: [
+          'rgba(244, 10, 10, 0.75)',
+          'rgba(248, 129, 32, 0.75)',
+          'rgba(247, 216, 43, 0.75)', 
+          'rgba(248, 243, 124, 0.75)',
+          'rgba(128, 248, 124, 0.75)',
+          'rgba(13, 160, 44, 0.75)',
+          'rgba(157, 250, 233, 0.75)',
+          'rgba(51, 199, 244, 0.75)',
+          'rgba(9, 52, 239, 0.75)',
+          'rgba(141, 27, 247, 0.75)',
+          'rgba(200, 27, 247, 0.75)',
+          'rgba(249, 51, 206, 0.75)',
+          ],
+        data: [...dataCounts, 0]
+      }]
+    },
+    options: {}
+  });
+
+}
